08a2ea5df421a37e413d6485d7d47b2c3aa0b0c6
pedro <phpmussi@gmail.com>
2022-05-29 10:13:11 -0300

    First Commit

---
+++ validador.py
@@ -0,0 +1,32 @@
+def cpf_validate(cpf):
+    # verify if its only numbers and turning each iterable character into an int so that the isdigit function works:
+    cpf = [int(char) for char in cpf if char.isdigit()]
+
+    # verify if has 11 digits:
+    if len(cpf) == 11:
+        pass
+    else:
+        return False
+
+    # cross out exception cases (111111111111,22222222222,[...]) :
+    if cpf == cpf[::-1]:
+        return False
+    else:
+        pass
+
+    #  Validate both verification digits:
+    for i in range(9, 11):
+        value = sum(cpf[num] * ((i + 1) - num) for num in range(0, i))
+        digito = ((value * 10) % 11) % 10
+        if digito != cpf[i]:
+            return False
+        else:
+            return True
+
+
+cpf = input("digite o cpf : ")
+
+if cpf_validate(cpf):
+    print("valido")
+else:
+    print("invalido")
